a7451fe6c7805fab7f878c61d45b7130e7238576
YOONJUNG HWANG <101488338+redcarrot-h@users.noreply.github.com>
2022-05-19 00:50:25 +0900

    프로그래머스 12937

---
+++ pg12937.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//@Url : https://programmers.co.kr/learn/courses/30/lessons/12937
+
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if (num % 2 == 0) {
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
